3461adb3affc7406493ad62e748fc8355bf48fd1
arojases <arielalejandrorojas@gmail.com>
2021-01-28 10:17:04 -0700

    Updated by Ariel

--- src/stratton/Main.java
+++ src/stratton/Main.java
@@ -3,8 +3,12 @@ package stratton;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Heather");
-        
-       System.out.println("Hello Heather, my name is Liz");
+
+        System.out.println("Hello Heather, my name is Liz");
+
+        System.out.println("Hello Heather, my name is Ariel");
+
     }
 }
